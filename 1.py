6bc0695c4334266e2f8492be99d00d4d4f1604af
Minjae Jin <moc0etan@gmail.com>
2019-03-08 16:56:06 +0900

    190308

--- 1.py
+++ 1.py
@@ -2,57 +2,46 @@ import sys
 
 sys.stdin = open('input.txt', 'r')
 
-T = int(input())
-for t in range(T):
-    N, L, k = map(int, input().split())
-    stick = [0]*(L+1)
-    for _ in range(N):
-        p, a = map(int, input().split())
-        stick[p] = a
 
-    for i in range((L+1)//2+1):
-        if stick[i] > 0:
+def safe(y, x):
+    if 0 < y < 100 and 0 <= x < 100 and Data[y][x]:
+        return True
+    return False
 
 
-        elif stick[L-i]
+def dfs(y, x):
+    global count, min_count
+    if min_count < count:
+        return
+    for j in range(3):
+        if safe(y + dy[j], x + dx[j]):
+            if j != 2:
+                k = 1
+                while safe(y + dy[j]*k, x + dx[j]*k):
+                    count += 1
+                    k += 1
+                else:
+                    dfs(y + dy[j]*k + 1, x + dx[j]*k)
+                    break
+            else:
+                dfs(y + dy[j], x + dx[j])
 
 
-    #     ID.append(a)
-    #     if a > 0:
-    #         plus.append(L-p)
-    #         now = len(plus-1)
-    #         pre = now - 1
-    #         while plus[pre] > plus[now]:
-    #             plus[pre], plus[now] = plus[now], pre[now]
-    #             now = pre
-    #             pre = now -1
-    #
-    #     else:
-    #         minus.append(p-0)
-    #         if len(minus) > 2:
-    #         now = len(plus - 1)
-    #         pre = now - 1
-    #         while plus[pre] > plus[now]:
-    #             plus[pre], plus[now] = plus[now], pre[now]
-    #             now = pre
-    #             pre = now - 1
-    # K = 0
-    # while K < k:
-    #     if not plus or plus[0] > minus[0]:
-    #         answer = ID.pop(0)
-    #         minus.pop(0)
-    #         K += 1
-    #     elif not minus or plus[0] < minus[0]:
-    #         answer = ID.pop(-1)
-    #         plus.pop(0)
-    #         K += 1
-    #     else:
-    #         if abs(ID[0]) < abs(ID[-1]):
-    #             answer = ID.pop(0)
-    #             minus.pop(0)
-    #         else:
-    #             answer = ID.pop(-1)
-    #             plus.pop(0)
-    #         K += 1
-    #
-    # print(answer)
+dy = [0, 0, 1]
+dx = [1, -1, 0]
+for _ in range(10):
+    t = int(input())
+    Data = [0]*100
+    for d in range(100):
+        Data[d] = list(map(int, input().split()))
+
+    min_count = 987654321
+    for i in range(100):
+        if Data[0][i] == 1:
+            count = 0
+            dfs(1, i)
+            if min_count >= count:
+                min_count = count
+                min_index = i
+
+    print('#{} {}'.format(t, min_index))

f5a12dbdd3fd79ad9d5eac28568d5dbcb084e948
Minjae Jin <moc0etan@gmail.com>
2019-04-03 18:01:45 +0900

    190403

--- 2.py
+++ 2.py
@@ -1,21 +1,40 @@
 import sys
 sys.stdin = open('input2.txt')
 
+
+def subset(visit, k, n):
+    global N, min_sum
+    if n == N//2:
+        x = y = 0
+        for i in range(N):
+            if visit & (1<<i):
+                x += data[i][0]
+                y += data[i][1]
+            else:
+                x -= data[i][0]
+                y -= data[i][1]
+
+        if min_sum > x**2+y**2:
+            min_sum = x**2+y**2
+        return
+    elif m==N//2:
+
+    if k >= N:
+        return
+    if N-k+n < N//2:
+        return
+    subset(visit ^ (1<<k), k+1, n+1,m)
+    subset(visit, k+1, n,m+1)
+
+
 T = int(input())
 for t in range(T):
-    N, M = map(int, input().split())
-    data = [[0]*(N+1) for _ in range(N+1)]
-    for _ in range(M):
-        a, b = map(int, input().split())
-        data[a][b] = 1
-        data[b][a] = 1
+    N = int(input())
+    data = [() for _ in range(N)]
+    for n in range(N):
+        data[n] = tuple(map(int, input().split()))
 
-    answer = [0]*(N+1)
-    for i in range(2, N+1):
-        if data[1][i]:
-            answer[i] = 1
-            for j in range(2, N+1):
-                if data[i][j]:
-                    answer[j] = 1
-
-    print('#{} {}'.format(t+1, answer.count(1)))
+    visit = 0
+    min_sum = 9999999999999
+    subset(0, 0, 0)
+    print('#{} {}'.format(t+1, min_sum))
